3666d2e18096749030d77888756390c1c071857f
Vanessa Cristina <vanessa.cristina.ads@gmail.com>
2023-10-31 23:26:57 -0300

    alterado conforme tarefa

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FormulariosProjeto'`, () => {
+  it(`should have as title 'tarefa'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FormulariosProjeto');
+    expect(app.title).toEqual('tarefa');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('FormulariosProjeto app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tarefa app is running!');
   });
 });
